258106f315266e96fd123a58f31506183c9ea823
mahala1 <77061196+mahala1@users.noreply.github.com>
2022-04-04 22:00:30 +0200

    Update TCPclient.py

--- TCPclient.py
+++ TCPclient.py
@@ -1,5 +1,5 @@
 from socket import *
-serverName = '192.168.1.2'
+serverName = '10.0.2.15'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
