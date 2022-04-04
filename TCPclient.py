a9215172483f3226d3ce2806ae59f37009cae1a4
mahala1 <77061196+mahala1@users.noreply.github.com>
2022-04-04 20:26:31 +0200

    Update TCPclient.py

--- TCPclient.py
+++ TCPclient.py
@@ -1,1 +1,10 @@
- 
+from socket import *
+serverName = '192.168.1.2'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+messagetoserver = raw_input('Enter Message for Server: ')
+clientSocket.send(messagetoserver)
+replyfromserver = clientSocket.recv(1024)
+print 'Reply Message from Server: ', replyfromserver
+clientSocket.close()
